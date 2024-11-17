ae423fbf61f9c39869521f7848adcfa597fb3a64
David Dutra <144396143+dutra357@users.noreply.github.com>
2024-11-17 18:35:32 -0300

    Ajuste de diretórios. Spring.

---
+++ src/test/java/com/dutra/dscomerce/DscomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutra.dscomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
